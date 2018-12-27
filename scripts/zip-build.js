e93f4dd5e7a2e5c427eecb63a5201fd22859c501
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2018-12-27 20:39:22 +0200

    add zip-build script to speed upload version process

---
+++ scripts/zip-build.js
@@ -0,0 +1,25 @@
+const admZip = require('adm-zip');
+
+const zip = new admZip();
+
+
+const folderToZip = './build';
+const destZipPath = './build/url-decoder.zip';
+
+console.log(`
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                CREATE ZIP FROM BUILD FOLDER
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+`);
+
+
+console.log(`ZIP the folder: '${folderToZip}'...`);
+console.log(`to: '${destZipPath}'...`);
+
+zip.addLocalFolder(folderToZip);
+
+zip.writeZip(destZipPath, () => console.log(`
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                            DONE!
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+`));
